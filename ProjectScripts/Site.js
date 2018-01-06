e8ab884842bc28dcb00d69c6a684f03930523b56
Sixian Lin <davidlin006811@gmail.com>
2018-01-06 13:33:10 -0500

    Add files via upload

--- ProjectScripts/Site.js
+++ ProjectScripts/Site.js
@@ -1,7 +1,28 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $(document).ready(function(){
+      //  var imgWidth = $('.img-container img').width();
+      //  $('.img-container').width(imgWidth);
+      $('.deleteAlbum').on('click', function(e){
+        e.preventDefault();
+        var deleteLink = $(this).attr('href');
+        $.Zebra_Dialog("All pictures in the ablum will be delete with the ablum. Are you sure?", {
+               type: 'warning',
+               title: 'Warining Information',
+               buttons: [
+                   {caption: 'Confirm', callback: function(){
+                           window.location.replace(deleteLink);
+                   } },
+                   { caption: 'Cancel' }
+               ]
+        });
+    });
+    
+});
 
+function ShowDiaglogBox(content){
+    $.Zebra_Dialog(content, {
+        type: 'information',
+        title: 'Success Information',
+        buttons:[{caption: 'Ok'}]
+    });
+}
 
